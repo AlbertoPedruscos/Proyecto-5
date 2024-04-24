fb1ddc5ed2ac9fe575ed59350a8a2ffa405672ae
AlbertoPedruscos <126202427+AlbertoPedruscos@users.noreply.github.com>
2024-04-24 18:21:44 +0200

    Add files via upload

---
+++ database/migrations/2024_04_24_160624_create_tbl_chat_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('CreateMensajeTable', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('emisor');
+            $table->unsignedBigInteger('receptor');
+            $table->text('mensaje')->nullable();
+            $table->timestamps();
+
+            // Establecer las claves foráneas
+            $table->foreign('emisor')->references('id')->on('tbl_usuarios')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('receptor')->references('id')->on('tbl_usuarios')->onDelete('cascade')->onUpdate('cascade');
+
+            // Esto asegura que no haya duplicados de pares de usuarios
+            $table->unique(['emisor', 'receptor']);
+
+            // Puedes añadir columnas adicionales si lo necesitas
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('CreateMensajeTable');
+    }
+};
+
