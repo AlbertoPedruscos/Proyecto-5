be122c631e9d36c3b2ecdcf2c14945360281ec20
AlbertoPedruscos <126202427+AlbertoPedruscos@users.noreply.github.com>
2024-04-24 18:43:32 +0200

    Update 2024_04_24_160624_create_tbl_chat_table.php

--- database/migrations/2024_04_24_160624_create_tbl_chat_table.php
+++ database/migrations/2024_04_24_160624_create_tbl_chat_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('CreateMensajeTable', function (Blueprint $table) {
+        Schema::create('tbl_chat', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('emisor');
             $table->unsignedBigInteger('receptor');
