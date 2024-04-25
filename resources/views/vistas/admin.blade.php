b383935a5d0b4678e1033c83606e597ed744c281
iker catalan gomez <100007043.joan23@fje.edu>
2024-04-25 18:21:38 +0200

    Arreglos

---
+++ resources/views/vistas/admin.blade.php
@@ -0,0 +1,24 @@
+@if (session('id') && session('nombre') && session('apellidos') && session('email') && session('rol'))
+
+@extends('layouts.plantilla_header') 
+
+@section('title', 'Admin | MyControlPark') 
+@section('css') 
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+@endsection
+
+@section('content')
+
+
+@endsection
+
+@else
+    @php
+        session()->flash('error', 'Debes iniciar sesión para acceder a esta página');
+    @endphp
+
+    <script>
+        window.location = "{{ route('login') }}"; <!-- Redireccionar a la página de inicio de sesión -->
+    </script>
+@endif
+
