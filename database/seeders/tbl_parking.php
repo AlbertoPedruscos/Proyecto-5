30f41bdf05746b23f44eb43e9a5d0566f039de03
iker catalan gomez <100007043.joan23@fje.edu>
2024-04-30 15:49:55 +0200

    Párkings reales + coordenadas en tbl_parking

--- database/seeders/tbl_parking.php
+++ database/seeders/tbl_parking.php
@@ -14,17 +14,41 @@ class tbl_parking extends Seeder
     public function run(): void
     {
         DB::table('tbl_parking')->insert([
-            'nombre' => 'Canserra',
-            'latitud' => '4555',
-            'longitud' => '4555',
+            'nombre' => 'Parking 1',
+            'latitud' => '41.349536354143744',
+            'longitud' => '2.106697003108879',
             'id_empresa' => 1,
             'created_at' => now(),
             'updated_at' => now()
         ]);
         DB::table('tbl_parking')->insert([
-            'nombre' => 'Platos',
-            'latitud' => '4555',
-            'longitud' => '4555',
+            'nombre' => 'Parking 2',
+            'latitud' => '41.35010355977579',
+            'longitud' => '2.106182758240472',
+            'id_empresa' => 2,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('tbl_parking')->insert([
+            'nombre' => 'Parking 3',
+            'latitud' => '41.34915063858738',
+            'longitud' => '2.105719090230854',
+            'id_empresa' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('tbl_parking')->insert([
+            'nombre' => 'Parking 4',
+            'latitud' => '41.34780760150584', 
+            'longitud' => '2.1074169285400246',
+            'id_empresa' => 2,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('tbl_parking')->insert([
+            'nombre' => 'Parking 5',
+            'latitud' => '41.34659114964164',  
+            'longitud' => '2.1098135735856767',
             'id_empresa' => 2,
             'created_at' => now(),
             'updated_at' => now()
