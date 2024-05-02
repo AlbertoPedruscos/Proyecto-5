8058e7d1cef0d0538d89e0a922a90eb524e48a74
iker catalan gomez <100007043.joan23@fje.edu>
2024-05-02 17:19:39 +0200

    hamburguesa, y arreglos de los CSS

--- resources/views/vistas/mapa_admin.blade.php
+++ resources/views/vistas/mapa_admin.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts.plantilla_header') <!-- Extiende desde la plantilla base -->
+@extends('layouts.plantilla_header')
 
-@section('title', 'Mapa | MyControlPark') <!-- Título personalizado -->
+@section('title', 'Mapa | MyControlPark')
 
-@section('css') <!-- CSS adicional -->
-    <link rel="stylesheet" href="{{ asset('css/mapa_admin.css') }}"> <!-- CSS personalizado -->
-    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css"> <!-- CSS para Leaflet -->
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/mapa_admin.css') }}">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css">
 @endsection
 
 @section('content')
@@ -18,68 +18,100 @@
         </nav>
     </header>
 
-    <!-- Contenedor principal con Flexbox -->
     <div id="cont-principal">
-        <div id="cont-crud">
-            <h1 style="color: white; text-align: center; font-size: 35px; font-family:Helvetica; padding: 25px 0px;">Listado
-                de los Parkings</h1>
+        <div id="cont-crud" class="collapsed">
+            <div class="menu-toggle" id="menuToggle">
+                <span class="hamburger"></span>
+                <span class="hamburger"></span>
+                <span class="hamburger"></span>
+            </div>
 
-            <div>
-                <h3>Añadir parking</h3>
-                <form action="">
-                    <div>
-                        <label for="nombre">Nombre</label>
-                        <input type="text" name="nombre" id="nombre" placeholder="Nombre del parking">
-                    </div>
+            <div class="menu-content">
+                <h1>Listado de los Parkings:</h1>
 
-                    <div>
-                        <label para="latitud">Latitud</label>
-                        <input type="text" name="latitud" id="latitud" placeholder="Latitud del parking">
-                    </div>
+                <button type="button" class="fa-solid fa-plus" data-bs-toggle="modal" data-bs-target="#modal"
+                    id="icono-suma" style="color: green; border: 2px solid green; padding: 5px;"></button>
 
-                    <div>
-                        <label para="longitud">Longitud</label>
-                        <input type="text" name="longitud" id="longitud" placeholder="Longitud del parking">
-                    </div>
-
-                    <div>
-                        <label para="empresa">Empresa</label>
-                        <input type="empresa" name="empresa" id="empresa" placeholder="Empresa del parking">
-                    </div>
-
-                    <div>
-                        <input type="submit" name="btn-enviar" id="btn-enviar" value="Añadir">
-                    </div>
-                </form>
+                <div id="resultado"></div>
             </div>
         </div>
-        <div id="map" style="flex: 1; height: 100%;">
+
+        <div id="map" style="flex: 1; height: 100%;"></div>
+    </div>
+
+    <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modal" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modal">Añadir Parking</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div>
+                            <label for="nombre">Nombre:</label>
+                            <input type="text" name="nombre" id="nombre" placeholder="Nombre del parking">
+                        </div>
+
+                        <div>
+                            <label para="latitud">Latitud:</label>
+                            <input type="text" name="latitud" id="latitud" placeholder="Latitud del parking">
+                        </div>
+
+                        <div>
+                            <label para="longitud">Longitud:</label>
+                            <input type="text" name="longitud" id="longitud" placeholder="Longitud del parking">
+                        </div>
+
+                        <div>
+                            <label para="empresa">Empresa</label>
+                            <section>
+                                <select name="empresa" id="empresa">
+                                    <option value="" selected disabled>-- Selecciona una opción --</option>
+                                    <option value="empresa1">Empresa 1</option>
+                                    <option value="empresa2">Empresa 2</option>
+                                    <option value="empresa3">Empresa 3</option>
+                                </select>
+                            </section>
+                        </div>
+
+                        <div>
+                            <input type="submit" name="btn-enviar" id="btn-enviar" value="Añadir">
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
+
 @endsection
 
 @push('scripts')
-    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script> <!-- Script de Leaflet -->
+    <script src="https://unpkg.com/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 
     <script>
-        // Inicializar el mapa con coordenadas especificadas
         var map = L.map('map').setView([41.3497528271445, 2.1080974175773473], 18);
 
-        // Añadir la capa de OpenStreetMap
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; OpenStreetMap contributors'
         }).addTo(map);
 
-        // Añadir un marcador con las coordenadas especificadas
-        L.marker([41.34971957556145, 2.1076759794296467]).addTo(map)
+        L.marker([41.34971957556145, 2.1076759794296467]).addTo(map);
 
-        // Función para mostrar un popup al hacer clic en el mapa
         map.on('click', function(e) {
-            var latlng = e.latlng; // Coordenadas del clic
+            var latlng = e.latlng;
             L.popup()
-                .setLatLng(latlng) // Posición del popup
-                .setContent("Coordenadas: " + latlng.lat + ", " + latlng.lng) // Contenido del popup
-                .openOn(map); // Mostrar el popup
+                .setLatLng(latlng)
+                .setContent("Latitud: " + latlng.lat + " Longitud: " + latlng.lng)
+                .openOn(map);
+        });
+
+        document.getElementById('menuToggle').addEventListener('click', function() {
+            var contCrud = document.getElementById('cont-crud');
+            contCrud.classList.toggle('expanded'); // Alternar entre colapsado y expandido
         });
     </script>
 @endpush
