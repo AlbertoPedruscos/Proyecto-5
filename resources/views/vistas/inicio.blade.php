e22e84d69d1ed621c2ca73a83a3897ee8667e29e
iker catalan gomez <100007043.joan23@fje.edu>
2024-05-08 18:55:21 +0200

    JS aplicado

--- resources/views/vistas/inicio.blade.php
+++ resources/views/vistas/inicio.blade.php
@@ -98,4 +98,94 @@
 @endsection
 
 @push('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script>
+        function reservarNuevo() {
+            var csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute('content');
+            var nom_cliente = document.getElementById("nom_cliente").value;
+            var matricula = document.getElementById("matricula").value;
+            var marca = document.getElementById("marca").value;
+            var modelo = document.getElementById("modelo").value;
+            var color = document.getElementById("color").value;
+            var num_telf = document.getElementById("num_telf").value;
+            var email = document.getElementById("email").value;
+            var ubicacion_entrada = document.getElementById("ubicacion_entrada").value;
+            var ubicacion_salida = document.getElementById("ubicacion_salida").value;
+            var fecha_entrada = document.getElementById("fecha_entrada").value;
+            var fecha_salida = document.getElementById("fecha_salida").value;
+
+        // Definir el evento onload para manejar la respuesta del servidor
+        xhr.onload = function() {
+            if (xhr.status == 200) {
+                // Aquí puedes manejar la respuesta del servidor si es necesario
+                if (xhr.responseText === "ok") {
+                    Swal.fire(
+                        'Reservado!',
+                        '¡El vehiculo ha sido reservado!',
+                        'success'
+                    );
+                    form.reset();
+                } else {
+                    Swal.fire(
+                        'Error!',
+                        '¡Rellena los campos!',
+                        'error'
+                    );
+                }
+            } else {
+                console.log('Error al realizar la reserva:', xhr.responseText);
+            }
+        };
+
+            // Crear una nueva solicitud XMLHttpRequest
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', '/reservaO', true);
+            xhr.setRequestHeader('X-CSRF-TOKEN', csrfToken);
+
+            // Definir el evento onload para manejar la respuesta del servidor
+            xhr.onload = function() {
+                if (xhr.status == 200) {
+                    // Aquí puedes manejar la respuesta del servidor si es necesario
+                    console.log(xhr.responseText);
+                    Swal.fire(
+                        'Reservado!',
+                        '¡El vehiculo ha sido reservado!',
+                        'success'
+                    );
+                } else {
+                    console.log('Error al realizar la reserva:', xhr.responseText);
+                }
+            };
+
+            // Definir el evento onerror para manejar errores de red
+            xhr.onerror = function(error) {
+                console.error('Error de red al intentar realizar la reserva:', error);
+            };
+
+            // Enviar la solicitud con el FormData que contiene los datos del formulario y la imagen de la firma
+            xhr.send(formData);
+        }
+    </script>
+
+    <!-- JS NAVBAR -->
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            // Referencias al navbar y al header
+            var navbar = document.getElementById('navbar');
+            var header = document.getElementById('header');
+
+            // Función para controlar el evento de desplazamiento
+            window.addEventListener('scroll', function() {
+                var headerHeight = header.offsetHeight; // Altura del header
+                var scrollPos = window.scrollY; // Posición de desplazamiento
+
+                // Fijar el navbar si se ha desplazado más allá de la altura del header
+                if (scrollPos >= headerHeight) {
+                    navbar.classList.add('navbar-fixed');
+                } else {
+                    navbar.classList.remove('navbar-fixed');
+                }
+            });
+        });
+    </script>
 @endpush
